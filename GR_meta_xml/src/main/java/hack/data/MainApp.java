f84b38e46a1cece6156d2520e5c991ac2c51ad8c
io42630 <ivan@olexyn.com>
2021-11-12 22:18:40 +0100

    generify, add geschaeft

--- GR_meta_xml/src/main/java/hack/data/MainApp.java
+++ GR_meta_xml/src/main/java/hack/data/MainApp.java
@@ -1,27 +1,38 @@
 package hack.data;
 
 import ch.cmiag.cdws.dokumente.Dokument;
+import ch.cmiag.cdws.geschaefte.Geschaeft;
 import org.xml.sax.SAXException;
 
-import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainApp {
+public class  MainApp {
+
+    public static final Map<Class<?>, Unmarshaller> UNMARSHALLER_MAP = new HashMap<>();
+
+    static {
+        try {
+            UNMARSHALLER_MAP.put(Dokument.class, JAXBContext.newInstance(Dokument.class).createUnmarshaller());
+            UNMARSHALLER_MAP.put(Geschaeft.class, JAXBContext.newInstance(Geschaeft.class).createUnmarshaller());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String... args) throws JAXBException, SAXException, MalformedURLException {
-        JAXBContext jc = JAXBContext.newInstance(Dokument.class);
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
 
-
-        JAXBElement<Dokument> element = (JAXBElement<Dokument>) unmarshaller.unmarshal(Utils.getUrl("DokumenteOne", ".xml"));
-        Dokument doc = element.getValue();
+        Dokument dokument =  unmarshall(Dokument.class, "DokumenteOne").getValue();
+        Geschaeft geschaeft =  unmarshall(Geschaeft.class, "GeschaefteOne").getValue();
         int br = 0;
+    }
+
+    public static <T> JAXBElement<T> unmarshall(Class<T> t, String name) throws MalformedURLException, JAXBException {
+        return  (JAXBElement<T>) UNMARSHALLER_MAP.get(t).unmarshal(Utils.getUrl(name, ".xml"));
     }
 }
